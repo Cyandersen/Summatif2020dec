b6faf7412c8cb3bd3839911175ae2726fcb5321f
cy.andersen <cy.andersen@cybeles-macbook-air.local>
2020-10-21 15:55:55 -0400

    Misc

---
+++ Introduction/Facture.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Facture {
+	
+	public static void main(String[] args)
+	{
+		/*- declarer les variables
+		 * ======================
+		 * nom, premon, montantLivres, montantFruits
+		 * montantTaxes, montantDiscount, montantTotal
+		 * netAPlayer
+		 * Questions
+		 * ======================
+		 * Demander le prenom et le nom
+		 * demander montant de livres
+		 * demnder montant des fruits
+		 * Calculs
+		 * ======================
+		 * calculer les taxes
+		 * calculer montant Total
+		 * calculer la remise
+		 * calculer le net a payer
+		 * Afficher
+		 */
+		Scanner sc=new Scanner(System.in);
+	}
+
+}
